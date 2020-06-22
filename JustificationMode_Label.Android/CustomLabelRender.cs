3f36ec3ea147a6a863468c4bc468f5600802cc5b
Maybel13 <mayradelacruzalvarado08@gmail.com>
2020-06-21 20:00:13 -0500

    version 1.0

---
+++ JustificationMode_Label.Android/CustomLabelRender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Xamarin.Forms;
+using JustificationMode_Label.Helpers;
+using JustificationMode_Label.Droid;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomLabel), typeof(CustomLabelRender))]
+namespace JustificationMode_Label.Droid
+{
+    public class CustomLabelRender: LabelRenderer
+    {
+        public CustomLabelRender(Context context): base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+                Control.JustificationMode = Android.Text.JustificationMode.InterWord;
+        }
+    }
+}
---
+++ JustificationMode_Label/Helpers/CustomLabel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace JustificationMode_Label.Helpers
+{
+    public class CustomLabel: Label
+    {
+    }
+}
